8c18920adc3328f22a48b52955babf827142759b
Krojce <krauzik7@gmail.com>
2017-11-27 19:14:03 +0100

    Add files via upload

---
+++ config.php
@@ -0,0 +1,21 @@
+<?php
+
+
+
+// Define DB Params
+
+define("DB_HOST", "c108um.forpsi.com");
+
+define("DB_USER", "f97454");
+
+define("DB_PASS", "P7tQMsn");
+
+define("DB_NAME", "f97454");
+
+
+
+// Define URL
+
+define("ROOT_PATH", $_SERVER['DOCUMENT_ROOT']."/");
+
+define("ROOT_URL", "https://www.imjk.cz/");
